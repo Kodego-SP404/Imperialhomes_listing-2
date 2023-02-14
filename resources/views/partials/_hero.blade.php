81077b10089e920af3e9980f19a306d0144f6d68
John-WD31 <j.cabs8888@gmail.com>
2023-02-14 15:52:13 +0800

    InitailCommit

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,21 @@
+ 
+
+ <section class="h-screen flex flex-col bg-gray-600 bg-center bg-cover bg-blend-overlay bg-black/50"
+ style="background-image: url(images/hero-bg.jpg)">
+ <!-- ####################################### Start Hero Content ###################################### -->
+ <div class="flex-1 flex items-center container mx-auto px-7">
+    <div class="text-center mx-auto">
+      <h1 class="text-5xl text-amber-500 m-10 mb-10">
+        Your Property Is Our Priority
+      </h1>
+      <p class="text-md font-light text-amber-300 mt-5">
+        A small river named Duden flows by their place and supplies it with
+        the necessary regelialia. It is a paradisematic country, in which
+        roasted parts of sentences fly into your mouth.
+      </p>
+      <a class="px-10 py-3 mt-5 text-white bg-amber-500 hover:bg-amber-400 inline-block rounded-2xl" href="#">Learn
+        More <i class="fa-sharp fa-solid fa-arrow-right"></i></a>
+    </div>
+  </div>
+  <!-- End of Hero -->
+ </section>
